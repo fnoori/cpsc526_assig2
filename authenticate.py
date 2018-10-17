a1fea8bc0a1c19232ac9070edf2b5178537643bf
Farzam Noori <farzamn80@gmail.com>
2018-10-17 16:07:00 -0600

    First commit

---
+++ authenticate.py
@@ -0,0 +1,20 @@
+# Command line arguments:
+#   username
+#   password
+#
+#
+# after entering username/password:
+#   if password for username is correct:
+#       print "access granted"
+#       exit code 0
+#   else
+#       print "access denied"
+#       exit code -1
+#
+#*******************************************************************
+
+def main:
+    print("Hello, World")
+
+
+main()
---
+++ enroll.py
@@ -0,0 +1,27 @@
+# Command line arguments:
+#   username:
+#       username can't be previously enrolled
+#
+#   password:
+#       password is not simple:
+#           "[word], [num], [wordnum], and [numword]" are forbidden
+#
+#
+#   if username/passwrod confrom to the requirements:
+#       print "accepted"
+#       username/password stored in password file
+#       exit code 0
+#       
+#   otherwise:
+#       print "rejected"
+#       exit code -1
+#
+#
+#*******************************************************************
+
+
+def main:
+    print("Hello, World")
+
+
+main()
