3703745b3eaa297eeb97c82a812a11b6eac22870
Farzam Noori <farzamn80@gmail.com>
2018-10-17 16:46:35 -0600

    check if password is an english word

--- enroll.py
+++ enroll.py
@@ -16,12 +16,34 @@
 #       print "rejected"
 #       exit code -1
 #
+#   Developed by:       Farzam Noori
+#   UCID:               10121495
+#   Course:             CPSC 526 - Network Security
+#   Tutorial:           03
+#   Assignemnt:         2 (Programming Part)
+#
+#   This program takes 2 command line arguments:
+#       argv[1]: username
+#       argv[2]: password
+#
 #
 #*******************************************************************
 
+import sys
+import enchant
 
 def main():
-    print("Hello, World")
+    username = sys.argv[1]
+    password = sys.argv[2]
 
+    checkPassword(sys.argv[2])
+
+def checkPassword(password):
+    wordChecker = enchant.Dict("en_US")
+
+    if (wordChecker.check(password)):
+        print("That's a word !")
+    else:
+        print("That's not a word !")
 
 main()
